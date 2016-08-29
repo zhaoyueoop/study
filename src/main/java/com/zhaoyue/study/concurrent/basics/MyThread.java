4ddc873c2db763a871eeae6241b760ad3e9d540f
zhaoyue <zhaoyue@yimayholiday.com>
2016-08-29 23:49:35 +0800

    基础代码提交

---
+++ src/main/java/com/zhaoyue/study/concurrent/basics/MyThread.java
@@ -0,0 +1,9 @@
+package com.zhaoyue.study.concurrent.basics;
+
+/**
+ * Created by zhaoy on 2016/8/29.
+ */
+public class MyThread {
+
+
+}
---
+++ src/test/java/com/zhaoyue/study/BaseJunit.java
@@ -0,0 +1,9 @@
+package com.zhaoyue.study;
+
+/**
+ * Created by zhaoy on 2016/8/29.
+ */
+public class BaseJunit {
+
+    
+}
